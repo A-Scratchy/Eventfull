d0352fd3a54a4482f73f6085c7b5387d2bd8e7ef
Adam Scratcherd <ascratcherd@gmail.com>
2021-03-28 18:01:44 +0100

    Added angular UI

---
+++ UI/ClientApp/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+
+import {AppComponent} from './app.component';
+import {NavMenuComponent} from './nav-menu/nav-menu.component';
+import {HomeComponent} from './home/home.component';
+import {CounterComponent} from './counter/counter.component';
+import {FetchDataComponent} from './fetch-data/fetch-data.component';
+import {OrdersComponent} from "./Orders/orders.component";
+import {OrderFormComponent} from "./Orders/OrderFormComponent";
+import {InputTextModule} from "primeng/inputtext";
+import {ButtonModule} from "primeng/button";
+import {TableModule} from "primeng/table";
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        HomeComponent,
+        CounterComponent,
+        FetchDataComponent,
+        OrdersComponent,
+        OrderFormComponent
+    ],
+    imports: [
+        BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
+        HttpClientModule,
+        FormsModule,
+        RouterModule.forRoot([
+            {path: '', component: HomeComponent, pathMatch: 'full'},
+            {path: 'counter', component: CounterComponent},
+            {path: 'fetch-data', component: FetchDataComponent},
+            {path: 'orders', component: OrdersComponent},
+        ]),
+        InputTextModule,
+        ButtonModule,
+        TableModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
